16431abbb02b70ebd27dba8cc9ff1cbe22147ff7
rbarb5 <b6010468@my.shu.ac.uk>
2017-04-26 15:40:13 +0100

    Add project files.

---
+++ KeyboardFinal/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace KeyboardFinal
+{
+    partial class Keyboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterBtn = new System.Windows.Forms.Button();
+            this.hashBtn = new System.Windows.Forms.Button();
+            this.zeroBtn = new System.Windows.Forms.Button();
+            this.starBtn = new System.Windows.Forms.Button();
+            this.emptyBtn2 = new System.Windows.Forms.Button();
+            this.threeBtn = new System.Windows.Forms.Button();
+            this.twoBtn = new System.Windows.Forms.Button();
+            this.oneBtn = new System.Windows.Forms.Button();
+            this.sixBtn = new System.Windows.Forms.Button();
+            this.fiveBtn = new System.Windows.Forms.Button();
+            this.fourBtn = new System.Windows.Forms.Button();
+            this.emptyBtn = new System.Windows.Forms.Button();
+            this.nineBtn = new System.Windows.Forms.Button();
+            this.eightBtn = new System.Windows.Forms.Button();
+            this.sevenBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.builderBox = new System.Windows.Forms.RichTextBox();
+            this.statusBox = new System.Windows.Forms.TextBox();
+            this.notePadBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // enterBtn
+            // 
+            this.enterBtn.Location = new System.Drawing.Point(320, 384);
+            this.enterBtn.Name = "enterBtn";
+            this.enterBtn.Size = new System.Drawing.Size(49, 100);
+            this.enterBtn.TabIndex = 43;
+            this.enterBtn.Text = "Enter";
+            this.enterBtn.UseVisualStyleBackColor = true;
+            // 
+            // hashBtn
+            // 
+            this.hashBtn.Location = new System.Drawing.Point(265, 437);
+            this.hashBtn.Name = "hashBtn";
+            this.hashBtn.Size = new System.Drawing.Size(49, 47);
+            this.hashBtn.TabIndex = 42;
+            this.hashBtn.Text = "#";
+            this.hashBtn.UseVisualStyleBackColor = true;
+            // 
+            // zeroBtn
+            // 
+            this.zeroBtn.Location = new System.Drawing.Point(210, 437);
+            this.zeroBtn.Name = "zeroBtn";
+            this.zeroBtn.Size = new System.Drawing.Size(49, 47);
+            this.zeroBtn.TabIndex = 41;
+            this.zeroBtn.Text = "0";
+            this.zeroBtn.UseVisualStyleBackColor = true;
+            // 
+            // starBtn
+            // 
+            this.starBtn.Location = new System.Drawing.Point(155, 437);
+            this.starBtn.Name = "starBtn";
+            this.starBtn.Size = new System.Drawing.Size(49, 47);
+            this.starBtn.TabIndex = 40;
+            this.starBtn.Text = "*-_";
+            this.starBtn.UseVisualStyleBackColor = true;
+            // 
+            // emptyBtn2
+            // 
+            this.emptyBtn2.Location = new System.Drawing.Point(320, 331);
+            this.emptyBtn2.Name = "emptyBtn2";
+            this.emptyBtn2.Size = new System.Drawing.Size(49, 47);
+            this.emptyBtn2.TabIndex = 39;
+            this.emptyBtn2.UseVisualStyleBackColor = true;
+            // 
+            // threeBtn
+            // 
+            this.threeBtn.Location = new System.Drawing.Point(265, 384);
+            this.threeBtn.Name = "threeBtn";
+            this.threeBtn.Size = new System.Drawing.Size(49, 47);
+            this.threeBtn.TabIndex = 38;
+            this.threeBtn.Text = "3\r\nWXYZ\r\n";
+            this.threeBtn.UseVisualStyleBackColor = true;
+            // 
+            // twoBtn
+            // 
+            this.twoBtn.Location = new System.Drawing.Point(210, 384);
+            this.twoBtn.Name = "twoBtn";
+            this.twoBtn.Size = new System.Drawing.Size(49, 47);
+            this.twoBtn.TabIndex = 37;
+            this.twoBtn.Text = "2\r\nTUV";
+            this.twoBtn.UseVisualStyleBackColor = true;
+            // 
+            // oneBtn
+            // 
+            this.oneBtn.Location = new System.Drawing.Point(155, 384);
+            this.oneBtn.Name = "oneBtn";
+            this.oneBtn.Size = new System.Drawing.Size(49, 47);
+            this.oneBtn.TabIndex = 36;
+            this.oneBtn.Text = "1\r\nPQRS";
+            this.oneBtn.UseVisualStyleBackColor = true;
+            // 
+            // sixBtn
+            // 
+            this.sixBtn.Location = new System.Drawing.Point(265, 331);
+            this.sixBtn.Name = "sixBtn";
+            this.sixBtn.Size = new System.Drawing.Size(49, 47);
+            this.sixBtn.TabIndex = 35;
+            this.sixBtn.Text = "6\r\nMNO";
+            this.sixBtn.UseVisualStyleBackColor = true;
+            // 
+            // fiveBtn
+            // 
+            this.fiveBtn.Location = new System.Drawing.Point(210, 331);
+            this.fiveBtn.Name = "fiveBtn";
+            this.fiveBtn.Size = new System.Drawing.Size(49, 47);
+            this.fiveBtn.TabIndex = 34;
+            this.fiveBtn.Text = "5\r\nJKL\r\n";
+            this.fiveBtn.UseVisualStyleBackColor = true;
+            // 
+            // fourBtn
+            // 
+            this.fourBtn.Location = new System.Drawing.Point(155, 331);
+            this.fourBtn.Name = "fourBtn";
+            this.fourBtn.Size = new System.Drawing.Size(49, 47);
+            this.fourBtn.TabIndex = 33;
+            this.fourBtn.Text = "4\r\nGHI";
+            this.fourBtn.UseVisualStyleBackColor = true;
+            // 
+            // emptyBtn
+            // 
+            this.emptyBtn.Location = new System.Drawing.Point(320, 278);
+            this.emptyBtn.Name = "emptyBtn";
+            this.emptyBtn.Size = new System.Drawing.Size(49, 47);
+            this.emptyBtn.TabIndex = 32;
+            this.emptyBtn.UseVisualStyleBackColor = true;
+            // 
+            // nineBtn
+            // 
+            this.nineBtn.Location = new System.Drawing.Point(265, 278);
+            this.nineBtn.Name = "nineBtn";
+            this.nineBtn.Size = new System.Drawing.Size(49, 47);
+            this.nineBtn.TabIndex = 31;
+            this.nineBtn.Text = "9\r\nDEF\r\n";
+            this.nineBtn.UseVisualStyleBackColor = true;
+            // 
+            // eightBtn
+            // 
+            this.eightBtn.Location = new System.Drawing.Point(210, 278);
+            this.eightBtn.Name = "eightBtn";
+            this.eightBtn.Size = new System.Drawing.Size(49, 47);
+            this.eightBtn.TabIndex = 30;
+            this.eightBtn.Text = "8\r\nABC";
+            this.eightBtn.UseVisualStyleBackColor = true;
+            // 
+            // sevenBtn
+            // 
+            this.sevenBtn.Location = new System.Drawing.Point(155, 278);
+            this.sevenBtn.Name = "sevenBtn";
+            this.sevenBtn.Size = new System.Drawing.Size(49, 47);
+            this.sevenBtn.TabIndex = 29;
+            this.sevenBtn.Text = "7\r\n.,\"\r\n";
+            this.sevenBtn.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(320, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 47);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "Send Predicted";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 47);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "Previous";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(210, 225);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 47);
+            this.button4.TabIndex = 26;
+            this.button4.Text = "Next";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(155, 225);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 47);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "Mode";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // builderBox
+            // 
+            this.builderBox.Location = new System.Drawing.Point(173, 188);
+            this.builderBox.Name = "builderBox";
+            this.builderBox.Size = new System.Drawing.Size(100, 20);
+            this.builderBox.TabIndex = 44;
+            this.builderBox.Text = "";
+            // 
+            // statusBox
+            // 
+            this.statusBox.Location = new System.Drawing.Point(289, 188);
+            this.statusBox.Name = "statusBox";
+            this.statusBox.ReadOnly = true;
+            this.statusBox.Size = new System.Drawing.Size(61, 20);
+            this.statusBox.TabIndex = 45;
+            this.statusBox.Text = "Multi-Press";
+            // 
+            // notePadBox
+            // 
+            this.notePadBox.Location = new System.Drawing.Point(94, 47);
+            this.notePadBox.Name = "notePadBox";
+            this.notePadBox.Size = new System.Drawing.Size(352, 123);
+            this.notePadBox.TabIndex = 46;
+            this.notePadBox.Text = "";
+            // 
+            // Keyboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 561);
+            this.Controls.Add(this.notePadBox);
+            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.builderBox);
+            this.Controls.Add(this.enterBtn);
+            this.Controls.Add(this.hashBtn);
+            this.Controls.Add(this.zeroBtn);
+            this.Controls.Add(this.starBtn);
+            this.Controls.Add(this.emptyBtn2);
+            this.Controls.Add(this.threeBtn);
+            this.Controls.Add(this.twoBtn);
+            this.Controls.Add(this.oneBtn);
+            this.Controls.Add(this.sixBtn);
+            this.Controls.Add(this.fiveBtn);
+            this.Controls.Add(this.fourBtn);
+            this.Controls.Add(this.emptyBtn);
+            this.Controls.Add(this.nineBtn);
+            this.Controls.Add(this.eightBtn);
+            this.Controls.Add(this.sevenBtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Name = "Keyboard";
+            this.Text = "Keyboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enterBtn;
+        private System.Windows.Forms.Button hashBtn;
+        private System.Windows.Forms.Button zeroBtn;
+        private System.Windows.Forms.Button starBtn;
+        private System.Windows.Forms.Button emptyBtn2;
+        private System.Windows.Forms.Button threeBtn;
+        private System.Windows.Forms.Button twoBtn;
+        private System.Windows.Forms.Button oneBtn;
+        private System.Windows.Forms.Button sixBtn;
+        private System.Windows.Forms.Button fiveBtn;
+        private System.Windows.Forms.Button fourBtn;
+        private System.Windows.Forms.Button emptyBtn;
+        private System.Windows.Forms.Button nineBtn;
+        private System.Windows.Forms.Button eightBtn;
+        private System.Windows.Forms.Button sevenBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox builderBox;
+        private System.Windows.Forms.TextBox statusBox;
+        private System.Windows.Forms.RichTextBox notePadBox;
+    }
+}
+
